3d05fbfdaa438d4cf2d4f60726e63c6f83ddec72
Dylie Dylan <djfarock@gmail.com>
2020-01-09 18:43:09 +0100

    Adding solidity code + index

---
+++ index.js
@@ -0,0 +1,31 @@
+abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"votesTracking","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
+provider = new ethers.providers.Web3Provider(web3.currentProvider);
+signer = provider.getSigner(0);
+contract = new ethers.Contract('0xb204cd5e3ae836eebbb8ffdd300aaf3074a32407', abi, signer)
+
+candidates = {"Dylie": "candidate-1", "James": "candidate-2", "Primate": "candidate-3", "Fallen": "candidate-4", "Exlo": "candidate-5", "Test": "candidate-6"}
+
+function voteForCandidate(candidate) {
+ candidateName = $("#candidate").val();
+  console.log(candidateName);
+
+  contract.voteForCandidate(ethers.utils.formatBytes32String(candidateName)).then((f) => {
+    let div_id = candidates[candidateName];
+    contract.totalVotesFor(ethers.utils.formatBytes32String(candidateName)).then((f) => {
+      $("#" + div_id).html(f);
+    })
+  });
+}
+
+$(document).ready(function() {
+  
+ candidateNames = Object.keys(candidates);
+
+ for(var i=0; i<candidateNames.length; i++) {
+  let name = candidateNames[i];
+  contract.totalVotesFor(ethers.utils.formatBytes32String(name)).then((f) => {
+    $("#" + candidates[name]).html(f);
+  })
+ }
+});
